7b9b35a36473e892884bb24361e2f282404ee4ec
julienmalard <julien.malard@mail.mcgill.ca>
2021-02-16 15:18:45 -0500

    gh-pages

---
+++ precache-manifest.194e3aea677deb35e594e885fa21018e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aaf3bb64b0f8ed1ef7f",
+    "url": "/entewaweyentehtasheke/css/app.562ef27b.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/entewaweyentehtasheke/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "484805176676fb04fef9c92a7e0a42ae",
+    "url": "/entewaweyentehtasheke/index.html"
+  },
+  {
+    "revision": "76ad53c4274ec449de21",
+    "url": "/entewaweyentehtasheke/js/about.edfc4aff.js"
+  },
+  {
+    "revision": "4aaf3bb64b0f8ed1ef7f",
+    "url": "/entewaweyentehtasheke/js/app.f6428f27.js"
+  },
+  {
+    "revision": "3a40af2e2d602cbc470e",
+    "url": "/entewaweyentehtasheke/js/chunk-vendors.b05f29ef.js"
+  },
+  {
+    "revision": "f060a4937fdd61e31bf12b4cfc075339",
+    "url": "/entewaweyentehtasheke/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/entewaweyentehtasheke/robots.txt"
+  }
+]);
